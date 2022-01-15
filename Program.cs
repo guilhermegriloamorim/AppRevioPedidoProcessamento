7c99472e6c8f6358775204c2658b7ee3977866d7
Guilherme Grilo Amorim <guilherme.g.amorim@gmail.com>
2022-01-15 11:35:49 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using AppRevioPedidoProcessamento.Data;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
